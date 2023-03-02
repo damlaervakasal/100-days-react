d776ffffbe016b09f9deb7b8fb8d0c6ae6ff7b38
Damla Erva Kasal <damlaervakasal3@gmail.com>
2023-03-03 02:48:35 +0300

    updated

--- react-07-fetch/src/components/Users.js
+++ react-07-fetch/src/components/Users.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
+import axios from "axios";
+
 function Users() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data);
-      })
+    axios("https://jsonplaceholder.typicode.com/users")
+      // .then((res) => res.json()) axios kullandığımız için bu işleme ihtiyacımız kalmadı.
+      //  fetch kullansaydık bunu da kullanmamız gerekecekti. fetch i kullandığımız zaman bize body
+      //  json olarak değil de string ifade olarak veriyor. Axios ta ise direk bize obje olarak dönüyor.
+      .then((res) => setUsers(res.data))
       .catch((e) => console.log(e))
       .finally(() => setIsLoading(false));
   }, []);
