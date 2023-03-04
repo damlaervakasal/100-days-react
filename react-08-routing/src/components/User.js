76a9a4a9cb268e80e519ce3c6b15fa6ef780782e
Damla Erva Kasal <damlaervakasal3@gmail.com>
2023-03-04 16:23:11 +0300

    pulled a users with json

--- react-08-routing/src/components/User.js
+++ react-08-routing/src/components/User.js
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
 
 function User() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+
   const { id } = useParams();
+
+  useEffect(() => {
+    axios(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => setUser(res.data))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  // sadece mount olduğu anda çalışıyor
+
   return (
     <div>
       <h1>User Detail</h1>
-      id: {id}
+      {loading && <div> Loading...</div>}
+      {!loading && <code>{JSON.stringify(user)}</code>}
+      <br />
+      <br />
+      <Link to={`/user/${parseInt(id) + 1}`}>
+        Next User ({parseInt(id) + 1}){" "}
+      </Link>
+      {/* user id leri ardışık gittiği için böyle bir yazım şekli kullandık int
+      çevirerek bir sornarki id ye geçmesini sağladık */}
     </div>
   );
 }
--- react-08-routing/src/components/Users.js
+++ react-08-routing/src/components/Users.js
@@ -3,17 +3,19 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Users() {
+  const [loading, setLoading] = useState(true);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios("https://jsonplaceholder.typicode.com/users").then((res) =>
-      setUsers(res.data)
-    );
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div>
       <h1>Users</h1>
+      {loading && <div> Loading...</div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>
