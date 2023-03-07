f5a9a673123fb57953aca44ab1e0273a1c97f8b5
Damla Erva Kasal <damlaervakasal3@gmail.com>
2023-03-08 00:18:06 +0300

    formik installation done

---
+++ react-09-formik/src/App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Formik, Field, Form } from "formik";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          console.log(values);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
+          <br />
+          <br />
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+          <br />
+          <br />
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <br />
+          <br />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default App;
