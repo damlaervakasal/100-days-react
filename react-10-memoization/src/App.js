90b57f9b04a1a371e5b504925358190ce9184a0d
Damla Erva Kasal <damlaervakasal3@gmail.com>
2023-03-11 23:49:34 +0300

    used react.memo to avoid re-render

--- react-10-memoization/src/App.js
+++ react-10-memoization/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
   const [number, setNumber] = useState(0);
@@ -7,6 +8,11 @@ function App() {
     <div className="App">
       <h1>{number}</h1>
       <button onClick={() => setNumber(number + 1)}>Click</button>
+      <br />
+      <br />
+      <hr />
+      <br />
+      <Header number={number < 5 ? 0 : number} />
     </div>
   );
 }
